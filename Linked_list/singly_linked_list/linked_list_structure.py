cfd61a622e7125337db7fd179c85562ac51a7dc1
Aminu Iliyasu <iliyasuaminu50@gmail.com>
2026-05-09 23:23:44 +0200

    Linkedlist in a dictionary form

---
+++ Linked_list/singly_linked_list/linked_list_structure.py
@@ -0,0 +1,17 @@
+Head = {
+    "value":5,
+    "next": {
+        "value":10,
+        "next": {
+            "value":3,
+            "next": {
+                "value":4,
+                "next": {
+                    "value":7,
+                    "next": None
+                }
+        }   }
+    }
+}
+
+print(Head)
