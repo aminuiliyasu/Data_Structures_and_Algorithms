a9e248285f9028eb1398ffcc6cad06ff2f9dcfda
Aminu Iliyasu <iliyasuaminu50@gmail.com>
2026-05-12 12:23:42 +0200

    Prepend and pop first in doubly linkedlist

--- Linked_list/Doubly_linked_list/doubly_ll_constructor.py
+++ Linked_list/Doubly_linked_list/doubly_ll_constructor.py
@@ -43,6 +43,30 @@ class DoublyLinkedList:
             temp.prev = None
         self.length -= 1
         return temp.value
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+        self.length += 1
+        return True
+    def pop_first(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            self.head = self.head.next
+            self.head.prev = None
+            temp.next = None
+        self.length -= 1
+        return temp.value
 
 my_linked_list = DoublyLinkedList(1)
 my_linked_list.print_list()
