016c842253fa659281d25c736e4440e7123966b8
Aminu Iliyasu <iliyasuaminu50@gmail.com>
2026-05-17 23:20:48 +0200

    Graph, edge and vertex

---
+++ Graph/graph_constructor.py
@@ -0,0 +1,53 @@
+class Graph:
+    def __init__(self):
+        self.adj_list = {}
+
+    def print_graph(self):
+        for vertex in self.adj_list:
+            print(vertex, ':', self.adj_list[vertex])
+    def add_vertex(self,vertex):
+        if vertex not in self.adj_list.keys():
+            self.adj_list[vertex]=[]
+            return True
+        return False
+    def add_edge(self,vertex1,vertex2):
+        if vertex1 in self.adj_list.keys() and vertex2 in self.adj_list.keys():
+            self.adj_list[vertex1].append(vertex2)
+            self.adj_list[vertex2].append(vertex1)
+            return True
+        return False
+    def remove_edge(self,vertex1,vertex2):
+        if vertex1 in self.adj_list.keys() and vertex2 in self.adj_list.keys():
+            try:
+                self.adj_list[vertex1].remove(vertex2)
+                self.adj_list[vertex2].remove(vertex1)
+                return True
+            except ValueError:
+                return False
+        return False
+    def remove_vertex(self,vertex):
+        if vertex in self.adj_list.keys():
+            for adjacent_vertex in self.adj_list[vertex]:
+                self.adj_list[adjacent_vertex].remove(vertex)
+            del self.adj_list[vertex]
+            return True
+        return False
+
+
+
+
+
+my_graph = Graph()
+
+my_graph.add_vertex('A')
+my_graph.add_vertex('B')
+my_graph.add_vertex('C')
+my_graph.add_edge('A', 'B')
+my_graph.add_edge('A', 'C')     
+my_graph.add_edge('B', 'C')
+
+
+
+my_graph.print_graph()
+
+
