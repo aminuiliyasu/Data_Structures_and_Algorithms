7ec405ee9b8d2368e654a95462d984651da29b2e
Aminu Iliyasu <iliyasuaminu50@gmail.com>
2026-05-20 19:09:42 +0200

    Sink down method in heap

--- Heap/heap_constructor.py
+++ Heap/heap_constructor.py
@@ -33,6 +33,21 @@ class MaxHeap:
         self._heapify(0)
         return root
     
+    def _sink_down(self,index):
+        max_index=index
+        while True:
+            left_index=self._left_child(index)
+            right_index=self._right_child(index)
+            if left_index<len(self.heap) and self.heap[left_index]>self.heap[max_index]:
+                max_index=left_index
+            if right_index<len(self.heap) and  self.heap[right_index]>self.heap[max_index]:
+                max_index=right_index
+            if max_index!=index:
+                self._swap(max_index,index)
+                index=max_index
+            else:
+                return
+    
     
 myheap = MaxHeap()
 myheap.insert(99)
